148aaa57048cb9392768b078bf8db0412afc15ab
Stackbit Code Editor <projects@stackbit.com>
2021-08-02 14:02:03 +0000

    home.js: changed

--- src/layouts/home.js
+++ src/layouts/home.js
@@ -22,7 +22,7 @@ export default class Home extends React.Component {
             <article key={index} className="post">
                 <header className="post-header">
                     <h2 className="post-title">
-                        <Link href={postUrl}>{title}</Link>
+                        
                     </h2>
                     <div className="post-meta">
                         Published on <time className="published" dateTime={dateTimeAttr}>{formattedDate}</time>
